c65dccce983c4eba69856d9c7e8a5a54af5500a1
Antonio Jesús <antoniojesuspv99@gmail.com>
2023-04-01 16:42:53 +0200

    cambio pagina inicial

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         title: 'Material App',
         debugShowCheckedModeBanner: false,
-        initialRoute: 'pagina2',
+        initialRoute: 'pagina1',
         routes: {
           'pagina1': (_) => Pagina1Page(),
           'pagina2': (_) => Pagina2Page()
